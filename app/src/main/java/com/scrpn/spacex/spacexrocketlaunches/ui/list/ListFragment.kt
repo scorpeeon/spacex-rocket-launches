e0903c289998351e7ce0d522370b5e993e41b679
Marton Varga <varga.marton@autsoft.hu>
2018-11-13 10:46:51 +0100

    Show welcome dialog

--- app/src/main/java/com/scrpn/spacex/spacexrocketlaunches/ui/list/ListFragment.kt
+++ app/src/main/java/com/scrpn/spacex/spacexrocketlaunches/ui/list/ListFragment.kt
@@ -1,6 +1,7 @@
 package com.scrpn.spacex.spacexrocketlaunches.ui.list
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v4.content.ContextCompat
 import android.view.View
 import androidx.core.view.get
@@ -10,19 +11,47 @@ import hu.autsoft.rainbowcake.base.BaseFragment
 import hu.autsoft.rainbowcake.base.getViewModelFromFactory
 import hu.autsoft.rainbowcake.navigation.navigator
 import kotlinx.android.synthetic.main.fragment_list.*
+import android.support.v7.app.AlertDialog
+
 
 class ListFragment : BaseFragment<ListViewState, ListViewModel>(), RocketPreviewAdapter.Listener {
+
+    companion object {
+        private const val COMPLETED_ONBOARDING_PREF_NAME = "COMPLETED_ONBOARDING_PREF_NAME"
+    }
 
     override fun provideViewModel() = getViewModelFromFactory()
     override fun getViewResource() = R.layout.fragment_list
 
-    lateinit var adapter: RocketPreviewAdapter
+    private lateinit var adapter: RocketPreviewAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         setupToolbar()
         setupRecyclerView()
+
+        PreferenceManager.getDefaultSharedPreferences(context).apply {
+            if (!getBoolean(COMPLETED_ONBOARDING_PREF_NAME, false)) {
+                showWelcomeDialog()
+            }
+        }
+    }
+
+    private fun showWelcomeDialog() {
+        context?.let { context ->
+            val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+            builder.setTitle(getString(R.string.welcome))
+                .setMessage(getString(R.string.welcome_details))
+                .setPositiveButton(android.R.string.ok) { dialog, which ->
+                    PreferenceManager.getDefaultSharedPreferences(context).edit().apply {
+                        putBoolean(COMPLETED_ONBOARDING_PREF_NAME, true)
+                        apply()
+                    }
+                }
+                .show()
+        }
+
     }
 
     override fun onStart() {
@@ -60,7 +89,6 @@ class ListFragment : BaseFragment<ListViewState, ListViewModel>(), RocketPreview
                 R.id.action_filter_by_active -> {
                     it.isChecked = !it.isChecked
                     viewModel.setFilterByActive(it.isChecked)
-                    true
                 }
             }
             true
